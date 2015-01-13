3049a93a29d34d187c8d1508154ff8fce96f3b30
Erik Kessler <erik.kessler1@gmail.com>
2015-01-13 14:12:59 -0500

    Simple Service

--- app/src/main/java/com/inspiredo/tealmorning/MainActivity.java
+++ app/src/main/java/com/inspiredo/tealmorning/MainActivity.java
@@ -1,11 +1,14 @@
 package com.inspiredo.tealmorning;
 
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.SharedPreferences;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
@@ -75,8 +78,50 @@ public class MainActivity extends ActionBarActivity
                         mCurrentRow = new MeditationSessionModel(null, -1);
 
     private boolean     mIsPrev;
-    private MeditationSessionModel
-                        mPrevSession;
+
+    private SessionPlaybackService mBoundService;
+    private boolean mIsBound;
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            // This is called when the connection with the service has been
+            // established, giving us the service object we can use to
+            // interact with the service.  Because we have bound to a explicit
+            // service that we know is running in our own process, we can
+            // cast its IBinder to a concrete class and directly access it.
+            mBoundService = ((SessionPlaybackService.SessionBinder)service).getService();
+
+            // Tell the user about this for our demo.
+            Log.d("Service", "connected");
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            // This is called when the connection with the service has been
+            // unexpectedly disconnected -- that is, its process crashed.
+            // Because it is running in our same process, we should never
+            // see this happen.
+            mBoundService = null;
+            Log.d("Service", "disconnected");
+        }
+    };
+
+    void doBindService() {
+        // Establish a connection with the service.  We use an explicit
+        // class name because we want a specific service implementation that
+        // we know will be running in our own process (and thus won't be
+        // supporting component replacement by other applications).
+        bindService(new Intent(MainActivity.this,
+                SessionPlaybackService.class), mConnection, Context.BIND_AUTO_CREATE);
+        mIsBound = true;
+    }
+
+    void doUnbindService() {
+        if (mIsBound) {
+            // Detach our existing connection.
+            unbindService(mConnection);
+            mIsBound = false;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -142,6 +187,7 @@ public class MainActivity extends ActionBarActivity
         mUserEmail = prefs.getString("User Email", "kessler.penguin55@gmail.com");
 
         getJSON();
+        doBindService();
     }
 
 
@@ -288,8 +334,9 @@ public class MainActivity extends ActionBarActivity
 
     @Override
     protected void onDestroy() {
-        if (mMeditationSession != null) mMeditationSession.stop();
         super.onDestroy();
+        if (mMeditationSession != null) mMeditationSession.stop();
+        doUnbindService();
     }
 
     private void prepSections(JSONArray jsArray) {
