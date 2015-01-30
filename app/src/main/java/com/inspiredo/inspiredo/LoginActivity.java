8227a3ea157a98f5bd6426e8eee8a4a21301a135
Erik Kessler <erik.kessler1@gmail.com>
2015-01-30 13:10:15 -0500

    Add icon

--- app/src/main/java/com/inspiredo/inspiredo/LoginActivity.java
+++ app/src/main/java/com/inspiredo/inspiredo/LoginActivity.java
@@ -20,7 +20,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import com.inspiredo.tealmorning.R;
+import com.inspiredo.inspiredo.R;
 
 import org.json.JSONException;
 import org.json.JSONObject;
--- app/src/main/java/com/inspiredo/inspiredo/SessionAdapter.java
+++ app/src/main/java/com/inspiredo/inspiredo/SessionAdapter.java
@@ -7,7 +7,7 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-import com.inspiredo.tealmorning.R;
+import com.inspiredo.inspiredo.R;
 
 /**
  * Adapter for History of sessions
